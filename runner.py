27e103920839e2e56b22b8b82992584ca78d7bc9
eotles <eotles@gmail.com>
2014-08-14 00:45:12 +0200

    this is obnoxious

--- runner.py
+++ runner.py
@@ -32,7 +32,7 @@ def main():
     mapFilepath = args[4]
     
     selectedControlsList = casel(caseFilepath, contFilepath, contRatio, kicFilepath)
-    monFiles = monsterFormatter(caseFilepath, contFilepath, selectedControlsList, kicFilepath, mapFilepath)
+    monFiles = monsterFormatter(caseFilepath, contFilepath, kicFilepath, mapFilepath, selectedControlsList)
     
     #print(monFiles)
     print("run like this>>>> /project/EngelmanGroup/GAW19/MONSTER/scr/./MONSTER "+ 
